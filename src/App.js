ac87eb152816d4e8884b684ca748b43f287c4a6e
chenl101 <chenl15213@gmail.com>
2020-02-08 00:36:30 -0500

    added remove button and remove feature in chekout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 
 import './App.css';
@@ -11,8 +12,12 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.compoenent';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 
 import { setCurrentUser } from './redux/user/user.actions'; 
+import { selectCurrentUser } from './redux/user/user.selectors';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -47,6 +52,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -64,8 +70,8 @@ class App extends React.Component {
   }  
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
